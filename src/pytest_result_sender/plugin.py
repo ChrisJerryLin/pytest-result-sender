695810752f0fb488f31b575b34fd2d634009005b
sunlin <1120795496@qq.com>
2024-08-25 20:57:48 +0800

    增加代码质量管理工具

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 
+
 def pytest_configure():
     """
     在所有测试用例执行前调用。
     """
     print(f"{datetime.now()} pytest执行开始")
+
     print(1233456)
+
 
 def pytest_unconfigure():
     """
     在所有测试用例执行完之后调用。
     """
     print(f"{datetime.now()} pytest执行结束")
-
- 
